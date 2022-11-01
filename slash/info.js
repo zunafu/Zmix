c7bb6de81e3e8d3e73a85d5da1df3b9923a17265
Damdep <79859009+Damdep@users.noreply.github.com>
2022-11-01 14:45:23 +0800

    Add files via upload

--- slash/info.js
+++ slash/info.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const { MessageEmbed } = require("discord.js")
-
+const { EmbedBuilder } = require("discord.js")
 module.exports = {
 	data: new SlashCommandBuilder().setName("info").setDescription("Displays info about the currently playing song"),
 	run: async ({ client, interaction }) => {
@@ -16,7 +15,7 @@ module.exports = {
         const song = queue.current
 
 		await interaction.editReply({
-			embeds: [new MessageEmbed()
+			embeds: [new EmbedBuilder()
             .setThumbnail(song.thumbnail)
             .setDescription(`Currently Playing [${song.title}](${song.url})\n\n` + bar)
         ],
